6eb2195d2099c37d0abce684e04c172e67029ab0
ZhaoyangSu <su@suzhaoyang.local>
2012-11-27 09:45:37 +0800

    create project

---
+++ graduationProject/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  graduationProject
+//
+//  Created by ZhaoyangSu on 12-11-27.
+//  Copyright (c) 2012年 ZhaoyangSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ graduationProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  graduationProject
+//
+//  Created by ZhaoyangSu on 12-11-27.
+//  Copyright (c) 2012年 ZhaoyangSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
